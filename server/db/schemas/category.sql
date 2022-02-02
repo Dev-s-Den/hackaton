99ac819b3c02cb39e6d787df59640d20141cf0e5
BRB3D <samuraix221@hotmail.com>
2022-02-02 16:34:25 -0500

    category schema

---
+++ server/db/schemas/category.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user CASCADE
+CREATE TABLE category(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL, 
+  color varchar(255) NOT NULL, 
+  completed BOOLEAN DEFAULT, 
+);
