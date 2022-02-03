da149dbde91cea9d841be7aeef3d8873a71217cb
BRB3D <samuraix221@hotmail.com>
2022-02-02 22:30:27 -0500

    resetdb finalized

--- server/bin/resetdb.js
+++ server/bin/resetdb.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const fs = require('fs');
 const chalk = require('chalk');
 const Client = require('pg-native');
+const { Pool } = require('pg');
 
 //Pg connection setup
 const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
@@ -21,4 +22,25 @@ const runSchemaFiles = function() {
   }
 };
 
+const runSeedFiles = function() {
+  console.log(chalk.cyan(`-> Loading Seeds ...`));
+  const schemaFilenames = fs.readdirSync(`./db/seeds`);
+
+  for (const fn of schemaFilenames) {
+    const sql = fs.readFileSync(`./db/seeds/${chalk.green(fn)}`);
+    console.log(`\t-> Running ${chalk.green(fn)}`);
+    client.querySync(sql);
+  }
+};
+
+try {
+  console.log(`-> Connecting to PG using ${connectionString}...`)
+  client.connectSync(connectionString);
+  runSchemaFiles();
+  runSeedFiles();
+  client.end();
+} catch (err) {
+  console.error(chalk.red(`Failed due to error: ${err}`))
+  client.end();
+}
 
