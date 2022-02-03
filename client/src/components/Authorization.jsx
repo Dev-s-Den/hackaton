1cc6e03e1f375d50815ad538d9afcf98f84cca74
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-02 23:19:47 -0500

    Create Auth component and style files

---
+++ client/src/components/Authorization.jsx
@@ -0,0 +1,12 @@
+// Style
+import "./styles/Authorization.scss";
+
+export default function Auth(props) {
+  return (
+    <>
+      <div className="auth">
+        <h1 className="auth-title">Register</h1>
+      </div>
+    </>
+  );
+}
