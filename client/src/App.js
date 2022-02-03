ae9ffdc6b6c141000cd9cd64485204ebf2d871a9
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-02 22:52:53 -0500

    Add NavBar to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import './App.scss';
+
+import NavBar from './components/NavBav';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <h1>Welcome to Hackaton</h1>
       </header>
