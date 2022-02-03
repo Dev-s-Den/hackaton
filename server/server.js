9e6741d38974e8143aba8eacb7243ff5e9ab6eba
BRB3D <samuraix221@hotmail.com>
2022-02-02 20:45:29 -0500

    server file

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+
+// requiring routes go here:____________________________________
+
+
+
+
+const app = express();
+// app.use(logger('dev')); only if we are using morgan
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({
+  name: process.env.SESSION_NAME,
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]
+}))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes go here:_____________________________________________
+
+
+module.exports = app;
