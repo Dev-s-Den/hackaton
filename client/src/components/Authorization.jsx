89528e6cb2d6cb5e95eac1a6a0ce38aba165cc98
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-03 09:22:28 -0500

    Edit Auth component

--- client/src/components/Authorization.jsx
+++ client/src/components/Authorization.jsx
@@ -4,8 +4,29 @@ import "./styles/Authorization.scss";
 export default function Auth(props) {
   return (
     <>
-      <div className="auth">
-        <h1 className="auth-title">Register</h1>
+      <div className="home">
+        <section className="text">
+          <text>
+            Lost track of time and assignments? Can't keep track of due dates?
+          </text>
+          <text>
+            Join us today! Hackaton will help you manage your time, projects and
+            goals!
+          </text>
+        </section>
+        <section className="auth">
+          <form className="form-signin">
+            <button className="btn btn-primary">Sign in</button>
+          </form>
+          <section className="separator">
+            <hr className="divider"></hr>
+            <text>or</text>
+            <hr className="divider"></hr>
+          </section>
+          <form className="form-signup">
+            <button className="btn btn-primary">Sign up</button>
+          </form>
+        </section>
       </div>
     </>
   );
