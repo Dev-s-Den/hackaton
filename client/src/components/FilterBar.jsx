3d5318bc7cd420f51cedc280b6075ae4e2041260
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-04 11:51:27 -0500

    Create FilterBar component and style it

---
+++ client/src/components/FilterBar.jsx
@@ -0,0 +1,48 @@
+// Libraries
+import React from "react";
+
+// Style
+import "./styles/FilterBar.scss";
+
+// Components
+
+export default function FilterBar(props) {
+  return (
+    <React.Fragment>
+      <section className="filter_bar">
+        <ul className="categories">
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+          <li className="category">
+            <p>Category</p>
+          </li>
+        </ul>
+      </section>
+    </React.Fragment>
+  );
+}
