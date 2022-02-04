efc1d8f952c08359fd93fdf8da8edca14dcee34a
BRB3D <samuraix221@hotmail.com>
2022-02-03 21:53:09 -0500

    setup to view goals

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,12 +6,14 @@ import "./styles/Home.scss";
 
 // Components
 import Auth from "./Authorization";
+import Goal from './Goal';
 
 export default function Home(props) {
   return (
     <React.Fragment>
-      <h1 className="title">Welcome to Hackaton!</h1>
-      <Auth />
+      {/* <h1 className="title">Welcome to Hackaton!</h1>
+      <Auth /> */}
+      <Goal />
     </React.Fragment>
   );
 }
