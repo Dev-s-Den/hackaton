eb7983a91f1f73ace6b4a55a5d331cacb74f575a
BRB3D <samuraix221@hotmail.com>
2022-02-04 16:22:04 -0500

    db typos

--- server/bin/resetdb.js
+++ server/bin/resetdb.js
@@ -7,16 +7,17 @@ const Client = require('pg-native');
 const { Pool } = require('pg');
 
 //Pg connection setup
-const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const connectionString = process.env.DATABASE_URL ||
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 const client = new Client();
 
 //Loads the Schema files from db/schema
 const runSchemaFiles = function() {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync('./db/schema');
+  const schemaFilenames = fs.readdirSync('./db/schemas');
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./db/schemas/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
@@ -24,10 +25,10 @@ const runSchemaFiles = function() {
 
 const runSeedFiles = function() {
   console.log(chalk.cyan(`-> Loading Seeds ...`));
-  const schemaFilenames = fs.readdirSync(`./db/seeds`);
+  const schemaFilenames = fs.readdirSync('./db/seeds');
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/seeds/${chalk.green(fn)}`);
+    const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
