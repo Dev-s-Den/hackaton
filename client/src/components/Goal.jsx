ef7aee764ddf6b2ebceba7eb2074c3cbd295517e
BRB3D <samuraix221@hotmail.com>
2022-02-04 12:19:53 -0500

    goal setup

--- client/src/components/Goal.jsx
+++ client/src/components/Goal.jsx
@@ -6,13 +6,36 @@ import "./styles/Goal.scss";
 export default function Goal(props) {
 
   return(
-    <React.Fragment>
+    <>
       <section >
-        <div class='goal'>
-          <p> content of the goal</p>
-          <p class='time'>5 days remaining</p>
+        <div className='goal'>
+          <div className='goal-header'>
+            <p> content of the goal</p>
+          </div>
+          <div className='goal-content'>
+            <div className="category">
+              <label>
+                <input type='checkbox'/>
+                Resume
+              </label>           
+              <label>
+              <input type='checkbox'/>
+                Research
+              </label>
+              <label>
+              <input type='checkbox'/>
+              Cover Letter
+              </label>
+            </div>
+            <div className='progress'>
+              <p>{0/5}</p>
+            </div>
+          </div>
+          <div className='time'>
+            <p>5 days remaining</p>
+          </div>
         </div>
       </section>
-    </React.Fragment>
+    </>
   )
 }
