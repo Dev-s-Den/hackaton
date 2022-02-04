371cb03ac1cf2053ae87c860f218c6cb3271d68d
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-04 15:29:51 -0500

    Fix Home style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,11 +7,13 @@ import "./styles/Home.scss";
 // Components
 // import Auth from "./Authorization";
 import FilterBar from "./FilterBar";
+import Goal from "./Goal";
 
 export default function Home(props) {
   return (
     <React.Fragment>
       <FilterBar />
+      <Goal />
     </React.Fragment>
   );
 }
