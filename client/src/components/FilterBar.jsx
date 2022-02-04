969f390802ef658cf74f20645af8d7ae31fde8fa
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-04 15:29:17 -0500

    Fix FilterBar style

--- client/src/components/FilterBar.jsx
+++ client/src/components/FilterBar.jsx
@@ -5,42 +5,19 @@ import React from "react";
 import "./styles/FilterBar.scss";
 
 // Components
+import Category from "./Category";
+
+// Mock database
+const categories = ["Resume", "Code", "Jobs", "Applications"];
 
 export default function FilterBar(props) {
   return (
     <React.Fragment>
       <section className="filter_bar">
         <ul className="categories">
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
-          <li className="category">
-            <p>Category</p>
-          </li>
+          {categories.map((c) => (
+            <Category key={c} name={c} />
+          ))}
         </ul>
       </section>
     </React.Fragment>
