71d048583da05965e318f7f17dbde6735fe7333b
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-04 16:36:28 -0500

    Add more goals data and change background

--- client/src/components/Goal.jsx
+++ client/src/components/Goal.jsx
@@ -90,6 +90,123 @@ export default function Goal(props) {
             <p>5 days remaining</p>
           </div>
         </li>
+        <li className="goal">
+          <header className="goal-header">
+            <p className="goal-title">Goal Title</p>
+            <ul className="goal-categories">
+              {categories.map((category) => (
+                <Category key={category} name={category} />
+              ))}
+            </ul>
+          </header>
+          <section className="goal-tasks">
+            <ul className="goal-tasks-list">
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Resume
+                </label>
+              </li>
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Research
+                </label>
+              </li>
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Cover Letter
+                </label>
+              </li>
+            </ul>
+            <section className="goal-progress">
+              <p>Completed:</p>
+              <progress className="progress" value="32" max="100" />
+            </section>
+          </section>
+          <div className="time-remaining">
+            <p>5 days remaining</p>
+          </div>
+        </li>
+        <li className="goal">
+          <header className="goal-header">
+            <p className="goal-title">Goal Title</p>
+            <ul className="goal-categories">
+              {categories.map((category) => (
+                <Category key={category} name={category} />
+              ))}
+            </ul>
+          </header>
+          <section className="goal-tasks">
+            <ul className="goal-tasks-list">
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Resume
+                </label>
+              </li>
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Research
+                </label>
+              </li>
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Cover Letter
+                </label>
+              </li>
+            </ul>
+            <section className="goal-progress">
+              <p>Completed:</p>
+              <progress className="progress" value="32" max="100" />
+            </section>
+          </section>
+          <div className="time-remaining">
+            <p>5 days remaining</p>
+          </div>
+        </li>
+        <li className="goal">
+          <header className="goal-header">
+            <p className="goal-title">Goal Title</p>
+            <ul className="goal-categories">
+              {categories.map((category) => (
+                <Category key={category} name={category} />
+              ))}
+            </ul>
+          </header>
+          <section className="goal-tasks">
+            <ul className="goal-tasks-list">
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Resume
+                </label>
+              </li>
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Research
+                </label>
+              </li>
+              <li className="goal-task">
+                <label>
+                  <input type="checkbox" />
+                  Cover Letter
+                </label>
+              </li>
+            </ul>
+            <section className="goal-progress">
+              <p>Completed:</p>
+              <progress className="progress" value="32" max="100" />
+            </section>
+          </section>
+          <div className="time-remaining">
+            <p>5 days remaining</p>
+          </div>
+        </li>
       </ul>
     </>
   );
