21ab6145dde1de43c0ea5c90374a79e3c4f23d06
BRB3D <samuraix221@hotmail.com>
2022-02-04 19:10:53 -0500

    user post fix

--- server/db/queries/user.js
+++ server/db/queries/user.js
@@ -14,7 +14,7 @@ const getUser = async (email) => {
 const addUser = async (first_name, last_name, email, password) => {
   const values = [first_name, last_name, email, password];
   try {
-    const data = await dbConnection.query('INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *;', values);
+    const data = await dbConnection.query('INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *;', values);
     return data.rows;
   } catch (err) {
     console.error(err.message);
