d8439a441a65d86905a4351da4c97a57b28b88e7
BRB3D <samuraix221@hotmail.com>
2022-02-05 19:25:49 -0500

    query for goal update

--- server/db/queries/goal.js
+++ server/db/queries/goal.js
@@ -12,10 +12,30 @@ const getGoals = async (userId) => {
 }
 
 const addGoal = async (userID, name, content, goal_end) => {
-  const values = [userID, name, content, goal_end]
+  const values = [userID, name, content, goal_end];
   try {
     const data = await dbConnection.query(`INSERT INTO goal(
       user_id, name, content, goal_end) VALUES ($1, $2, $3, $4)`, values);
+  } catch (err) {
+    console.error(err.message);
+    return err.message;
+  }
+}
+
+const deleteGoal = async (goalID) => {
+  const values = [goalId];
+  try {
+    const data = await dbConnection.query(`DELETE FROM goal WHERE id=$1`, values)
+  } catch (err) {
+    console.error(err.message);
+    return err.message;
+  }
+}
+
+const updateTasks = async (goalID, content) => {
+  const values = [content, goalID];
+  try {
+    const data = await dbConnection.query(`UPDATE goal SET content = $1 WHERE id=$2`, values)
   } catch (err) {
     console.error(err.message);
     return err.message;
