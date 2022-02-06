6faa42dce3c63fd08e8fd995e90eaee844d6581a
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-05 22:50:49 -0500

    Fix goal schema and seed

--- server/db/seeds/04_goal.sql
+++ server/db/seeds/04_goal.sql
@@ -1,3 +1,3 @@
 INSERT INTO goal ( user_id, name, task_id, goal_end, completed )
 VALUES
-(1, 'Apply to Fortinet', 1, TO_DATE('17/03/2022', 'DD/MM/YYYY'), false)
+(1, 'Apply to Fortinet', '[{"name":"Send resume", "completed":false}, {"name":"Write cover letter", "completed":true}, {"name":"Research company", "completed":true}]', TO_DATE('17/03/2022', 'DD/MM/YYYY'), false)
