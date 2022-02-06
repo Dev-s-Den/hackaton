43a3cd33454032c4075a021c46e031a72485940e
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-06 04:39:04 -0500

    Add Extra Seeds

--- server/db/seeds/03_goal.sql
+++ server/db/seeds/03_goal.sql
@@ -1,3 +1,6 @@
 INSERT INTO goal ( user_id, name, goal_end, completed )
 VALUES
-(1, 'Apply to Fortinet', TO_DATE('17/03/2022', 'DD/MM/YYYY'), false)
+(1, 'Apply to Fortinet', TO_DATE('17/03/2022', 'DD/MM/YYYY'), false),
+(1, 'Apply to Apple', TO_DATE('17/04/2022', 'DD/MM/YYYY'), false),
+(2, 'Apply to Google', TO_DATE('17/05/2022', 'DD/MM/YYYY'), false),
+(2, 'Apply to FaceBook', TO_DATE('10/02/2022', 'DD/MM/YYYY'), false)
--- server/db/seeds/04_task.sql
+++ server/db/seeds/04_task.sql
@@ -2,4 +2,13 @@ INSERT INTO task ( name, completed, goal_id)
 VALUES
 ('Send resume', false, 1),
 ('Write cover letter', true, 1),
-('Research company', true, 1)
+('Research company', true, 1),
+('Send resume', false, 2),
+('Write cover letter', true, 2),
+('Research company', true, 2),
+('Send resume', false, 3),
+('Write cover letter', true, 3),
+('Research company', true, 3),
+('Send resume', false, 4),
+('Write cover letter', true, 4),
+('Research company', true, 4)
