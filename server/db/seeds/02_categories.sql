ab6246db70f187bfac9985288cdf2420c70c2ea5
BRB3D <samuraix221@hotmail.com>
2022-02-05 20:39:14 -0500

    table typos

--- server/db/seeds/02_categories.sql
+++ server/db/seeds/02_categories.sql
@@ -1,4 +1,4 @@
-INSERT INTO categories ( name, color )
+INSERT INTO category ( name, color )
 VAlUES
 ('Code', '#37c859'),
 ('Jobs', '#3584ca'),
---
+++ server/db/seeds/03_task.sql
@@ -0,0 +1,5 @@
+INSERT INTO task ( name, completed)
+VALUES
+('Send resume', false),
+('Write cover letter', true),
+('Research company', true)
